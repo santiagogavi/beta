9569798b4888500eef0655bbf11e77a86d267449
santi <elversantiago84@gamil.com>
2024-09-20 12:25:55 -0500

    santi

---
+++ api/config/db_connection.php
@@ -0,0 +1,34 @@
+<?php
+/**postgresSQLdatabase conectiom
+ * devealoper santi
+ */
+
+$host = "localhost";
+$port = "5432";
+$dbname = "beta";
+$user = "postgres";
+$password = "unicesmag";
+
+$data_connection = "
+host = $host
+port  = $port
+dbname = $dbname
+user = $user
+password = $password
+
+
+";
+
+$conn = pg_connect($data_connection);
+
+if (!$conn){
+die("connection failed: ". pg_last_error());
+
+}else{
+
+    echo "Connected successfully";
+ }
+pg_close($conn);
+
+
+?>
