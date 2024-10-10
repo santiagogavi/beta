991d9bcfdce786a44cfad2c29ff8cdeae2f3b387
santi <elversantiago84@gamil.com>
2024-10-10 11:57:44 -0500

    update_login

--- api/src/backend/singup.php
+++ api/src/backend/singup.php
@@ -5,13 +5,25 @@
  $pass = $_POST['passwd'];
  $enc_pass = md5($pass);
 
+ //validate if email already  exists
+
+$query = "select * from users where email = '$email'";
+$result = pg_query($conn,$query);
+ 
+
+ $row = pg_fetch_array($result);
+ if($row) {
+    echo "<script>alert('email_already_exists')</script>";
+    header('refresh:0; url=http://127.0.0.1/beta/api/src/register_form.html');
+    exit();
+ }
+
  $query = "insert into users (email,password) 
  VALUES ('$email', '$enc_pass')";
-
  $result = pg_query($conn, $query);
-
  if($result) {
-     echo "registration successfully";
+    echo "<script>alert('registration_success')</script>";
+    header('refresh:0; url=http://127.0.0.1/beta/api/src/login_form.html');
  } else {
      echo " registration failed";
  }
